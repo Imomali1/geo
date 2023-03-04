2ac8c70640cef689924a1d92d70411e6b61ed221
Imomali Ramazonov <101134058+Imomali1@users.noreply.github.com>
2023-03-04 23:35:11 +0300

    Update geo.sql

--- sql/geo.sql
+++ sql/geo.sql
@@ -3,33 +3,32 @@ DROP TABLE IF EXISTS geo_test.test_point;
 DROP SCHEMA IF EXISTS geo_test;
 
 CREATE SCHEMA geo_test;
+
 CREATE TABLE geo_test.test_point (
-                                     id integer,
-                                     task integer,
-                                     x numeric(10, 2),
-                                     y numeric(10, 2),
-                                     z numeric(10, 2),
-                                     PRIMARY KEY (id)
+  id integer,
+  task integer,
+  x numeric(10, 2),
+  y numeric(10, 2),
+  z numeric(10, 2),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE geo_test.task (
-                               id integer PRIMARY KEY,
-                               name varchar(256)
+  id integer PRIMARY KEY,
+  name varchar(256)
 );
 
 ALTER TABLE geo_test.test_point
-    ADD CONSTRAINT task
-        FOREIGN KEY (task)
-            REFERENCES geo_test.task (id);
+ADD CONSTRAINT task
+FOREIGN KEY (task)
+REFERENCES geo_test.task (id);
 
 CREATE INDEX index_name ON geo_test.test_point (
-                                                id, task
-    );
+  id, task
+);
 
-INSERT INTO
-    geo_test.task
-VALUES
-    (1, 'TODO');
+-- Without this insertion catchs error
+INSERT INTO geo_test.task VALUES (1, 'TODO');
 
 INSERT INTO
     geo_test.test_point
@@ -40,10 +39,10 @@ VALUES
 
 SELECT * FROM geo_test.test_point LIMIT 1 OFFSET 1;
 
--- CREATE EXTENSION postgis;
+CREATE EXTENSION postgis;
 
 ALTER TABLE geo_test.test_point
-    ADD COLUMN geom geometry(PointZ);
+ADD COLUMN geom geometry(PointZ);
 
 UPDATE geo_test.test_point
 SET geom = ST_MakePoint(x,y,z);
